81b385d8607333cb7f068f8023638adc2a9d23ff
Jennifer Ng <ka.man.jennifer.ng@gmail.com>
2020-08-11 23:58:23 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,10 @@
-var chocolateBars = ['snickers','hundred grand','kitkat','skittles']
+var chocolateBars = ['snickers','hundred grand','kitkat','skittles'];
+
+function addElementToBeginningOfArray(array, element) {
+  var newArray = array.unshift(element);
+  return newArray;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return array.unshift(element);
+}
